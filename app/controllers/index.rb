56641699a72755bb36eadb3df0e3aa4dac51dcc3
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 10:31:47 -0500

    created user landing page to create a new user

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,13 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/create_new_user' do
+  erb :create_new_user
+end
+
+post '/completion' do
+  @user = User.create(params[:user_input])
+end
+
+
